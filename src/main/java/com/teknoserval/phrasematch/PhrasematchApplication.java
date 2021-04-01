1bba64b267bb55cc1bff63c5ce7aa9c933d7f430
TeknoServal <teknoserval@gmail.com>
2021-04-01 16:24:29 -0600

    Initial commit

---
+++ src/main/java/com/teknoserval/phrasematch/PhrasematchApplication.java
@@ -0,0 +1,13 @@
+package com.teknoserval.phrasematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasematchApplication.class, args);
+	}
+
+}
